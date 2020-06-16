9a333018ef950db6452563cfdab9d582a480097e
amainfosoft <amainfosoft@gmail.com>
2020-06-16 23:21:50 +0530

    commits

--- Home/views.py
+++ Home/views.py
@@ -25,7 +25,7 @@ def sending_email(request):
         #susmsg.append(msg)
 
         #return render(request,'index.html',{'susmsg':susmsg})
-        return redirect('index.html',{'susmsg':susmsg})
+        return redirect('index',{'susmsg':susmsg})
     else:
         return render(request,'index.html')
 
