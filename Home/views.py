c178999dbed4453e85b727208c4d053e873f2ef4
amainfosoft <amainfosoft@gmail.com>
2020-06-16 23:09:44 +0530

    commits

--- Home/views.py
+++ Home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.core.mail import send_mail
 from django.contrib import messages
+from django.http import HttpResponse
 import emoji
 
 # Create your views here.
@@ -20,11 +21,11 @@ def sending_email(request):
         send_mail(subject,message,email,['amainfosoft@gmail.com'],fail_silently=False,)
         #reply_msg = 'We have Received your Email Successfully ' + emoji.emojize(":smiling face:")
         #messages.add_message(request,messages.success,'We have Received your Email successfully...')
-        msg = 'We have Received your Email Successfully, We will get back to you soon.'
-        susmsg.append(msg)
+        susmsg = 'We have Received your Email Successfully, We will get back to you soon.'
+        #susmsg.append(msg)
 
         #return render(request,'index.html',{'susmsg':susmsg})
-        return redirect('index')
+        return HttpResponse(susmsg)
     else:
         return render(request,'index.html')
 
