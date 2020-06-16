f5b293d2073e153c6f673fa6ea2a12ae3c8f6112
amainfosoft <amainfosoft@gmail.com>
2020-06-16 10:53:24 +0530

    commits

--- myproject/settings.py
+++ myproject/settings.py
@@ -124,8 +124,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+#MEDIA_URL = '/media/'
+#MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 # Activate Django-Heroku.
 #django_heroku.settings(locals())
